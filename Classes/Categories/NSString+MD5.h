ffa50e186c8a9910851370793d0c5a2b2c0c932f
Nathan Borror <nathan@nathanborror.com>
2013-10-13 12:13:30 -0700

    Adding a MD5 category for NSString

---
+++ Classes/Categories/NSString+MD5.h
@@ -0,0 +1,15 @@
+//
+//  NSString+MD5.h
+//  NBKit
+//
+//  Created by Nathan Borror on 9/7/13.
+//  Copyright (c) 2013 Nathan Borror. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (MD5)
+
+- (NSString *)MD5;
+
+@end

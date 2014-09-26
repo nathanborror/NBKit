e123b4cbca43e2ba203f612ecb384435da632a60
Nathan Borror <nathan@nathanborror.com>
2014-09-26 15:45:18 -0700

    Adding simple Regex class for making it easier to work with regular expressions.

---
+++ NBKit/NBKit/Regex.swift
@@ -0,0 +1,44 @@
+//
+//  Regex.swift
+//  NBKit
+//
+//  Created by Nathan Borror on 9/26/14.
+//  Copyright (c) 2014 Nathan Borror. All rights reserved.
+//
+// Inspired by: http://benscheirman.com/2014/06/regex-in-swift/
+//
+
+import Foundation
+
+class Regex {
+
+    let pattern:String
+    let expression:NSRegularExpression
+
+    init(_ pattern:String, options:NSRegularExpressionOptions) {
+        self.pattern = pattern
+
+        var error:NSError?
+        self.expression = NSRegularExpression(pattern: self.pattern, options: options, error: &error)
+    }
+
+    convenience init(pattern:String) {
+        self.init(pattern, options: NSRegularExpressionOptions.CaseInsensitive)
+    }
+
+    func replace(input:String, with:String) -> String {
+        return self.expression.stringByReplacingMatchesInString(input, options: NSMatchingOptions(0), range: NSMakeRange(0, countElements(input)), withTemplate: with)
+    }
+
+    func test(input:String) -> Bool {
+        let matches = self.expression.matchesInString(input, options: NSMatchingOptions(0), range: NSMakeRange(0, countElements(input)))
+        return matches.count > 0
+    }
+
+}
+
+infix operator =~ {}
+
+func =~ (input:String, pattern:String) -> Bool {
+    return Regex(pattern: pattern).test(input)
+}

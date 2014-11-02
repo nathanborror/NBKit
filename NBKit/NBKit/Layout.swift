e5d2f35977f78389aef4e732a9fbed28d33c0d98
Nathan Borror <nathan@nathanborror.com>
2014-11-02 11:07:54 -0800

    Improving the Layout class by exposing add() and adding update() so you can clear out constraints and add new ones.

--- NBKit/NBKit/Layout.swift
+++ NBKit/NBKit/Layout.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+public enum LayoutDirection {
+    case Horizontal
+    case Vertical
+}
+
 // A basic method for applying layout constraints to views.
 public class Layout {
 
@@ -16,16 +21,13 @@ public class Layout {
     public var constraints = [AnyObject]()
 
     /**
-        A Layout applies constraints to views using Auto Layout's visual
-        formatting language.
-
-        :param: views The UIViews involved in the constraints.
-        :param: with Constraints defined by auto-layout's visual
-            formatting language to be applied to the superview.
-        :param: metrics Optional metric values.
-        :param: options Formatting options using NSLayoutFormatOptions.
+        Creates a Layout for the given views and figures out the superview
+        to apply constraints to.
+    
+        :param: views A dictionary of views with corresponding keys to be used
+            with auto-layout's visual formatting language.
     */
-    public init(_ views:[String:AnyObject], with:[String], metrics:[String:Double]? = nil, options:NSLayoutFormatOptions = NSLayoutFormatOptions(0)) {
+    public init(_ views:[String:AnyObject]) {
         self.views = views
 
         for (key,view) in self.views {
@@ -36,63 +38,85 @@ public class Layout {
                 self.superview = superview
             }
         }
+    }
 
-        self.addConstraints(with, metrics: metrics, options: options)
+    /**
+        Applies constraints to views using Auto-Layout's visual formatting language.
+
+        :param: formats Constraints defined by auto-layout's visual formatting
+            language to be applied to the superview.
+        :param: metrics Optional metric values.
+        :param: options Formatting options using NSLayoutFormatOptions.
+    
+        :returns: A reference to the Layout object.
+    */
+    public func with(formats:[String], metrics:[String:Double]? = nil, options:NSLayoutFormatOptions = NSLayoutFormatOptions(0)) -> Layout {
+        for format in formats {
+            self.add(format, metrics: metrics, options: options)
+        }
+        return self
     }
 
     /**
         A method for adding more constraints to the superview.
 
-        :param: with Constraints defined by auto-layout's visual
+        :param: format Constraints defined by auto-layout's visual
             formatting language to be applied to the superview.
         :param: metrics Optional metric values.
         :param: options Formatting options using NSLayoutFormatOptions.
+    
+        :returns: A reference to the Layout object.
     */
-    func addConstraint(with:String, metrics:[String: AnyObject]? = nil, options: NSLayoutFormatOptions = NSLayoutFormatOptions(0)) {
-        let constraint = NSLayoutConstraint.constraintsWithVisualFormat(with, options: options, metrics: metrics, views: self.views)
-
-        if superview != nil {
-            self.superview!.addConstraints(constraint)
-            self.constraints += constraint
-        }
-    }
-
-    /**
-        A method for adding constraints to a superview.
-
-        :param: with Constraints defined by auto-layout's visual
-            formatting language to be applied to the superview.
-        :param: options Formatting options using NSLayoutFormatOptions.
-        :param: metrics Optional metric values.
-    */
-    func addConstraints(with:[String], metrics:[String: AnyObject]? = nil, options: NSLayoutFormatOptions = NSLayoutFormatOptions(0)) {
-        for format in with {
-            self.addConstraint(format, metrics: metrics, options: options)
-        }
+    public func add(format:String, metrics:[String: AnyObject]? = nil, options: NSLayoutFormatOptions = NSLayoutFormatOptions(0)) -> Layout {
+        let constraint = NSLayoutConstraint.constraintsWithVisualFormat(format, options: options, metrics: metrics, views: self.views)
+        self.superview?.addConstraints(constraint)
+        self.constraints += constraint
+        return self
     }
 
     // MARK: Experimental
+
+    public class func max(direction:LayoutDirection, obj:AnyObject) -> Layout {
+        switch direction {
+        case .Horizontal:
+            return Layout(["obj": obj]).add("H:|[obj]|")
+        case .Vertical:
+            return Layout(["obj": obj]).add("V:|[obj]|")
+        }
+    }
 
     /**
         A convenience method for when you want a view to maximize the space
         afforded by its superview.
 
         :param: view The view you want to fill its superview.
+    
+        :returns: A reference to the Layout object.
     */
     public class func max(obj:AnyObject) -> Layout {
-        let layout = Layout(["obj": obj], with: [
-            "H:|[obj]|",
-            "V:|[obj]|",
-        ])
-        return layout
+        return Layout(["obj": obj])
+            .add("H:|[obj]|")
+            .add("V:|[obj]|")
     }
 
     /**
-        A convenience method that returns the last constraint added to a
-        superview.
+        Returns the last constraint added to a superview.
     */
     public func last() -> NSLayoutConstraint! {
         return self.constraints.last as NSLayoutConstraint
+    }
+
+    /**
+        Updates constraints by removing all prior constraints and adding
+        newly provided ones.
+    
+        :param: formats The new constraints to be applied to the superview.
+    
+        :returns: A reference to the Layout object.
+    */
+    public func update(formats:[String]) -> Layout  {
+        self.superview?.removeConstraints(self.constraints)
+        return self.with(formats)
     }
 
 }

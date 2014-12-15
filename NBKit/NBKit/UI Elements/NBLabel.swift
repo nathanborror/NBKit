98dd0089f2cded383d8d83fc2b50ff27301f5b2c
Nathan Borror <nathan@nathanborror.com>
2014-12-15 13:37:47 -0800

    Making NBLabel actually work

--- NBKit/NBKit/UI Elements/NBLabel.swift
+++ NBKit/NBKit/UI Elements/NBLabel.swift
@@ -10,28 +10,24 @@ import UIKit
 
 class NBLabel: UILabel {
 
-    let padding:UIEdgeInsets!
-
-    init(padding: UIEdgeInsets) {
-        super.init(frame: CGRectZero)
-        self.padding = padding
-    }
-
-    required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    var padding:UIEdgeInsets?
 
     override func drawTextInRect(rect: CGRect) {
-        return super.drawTextInRect(UIEdgeInsetsInsetRect(rect, padding))
+        if let padding = self.padding {
+            return super.drawTextInRect(UIEdgeInsetsInsetRect(rect, padding))
+        }
     }
 
     override func textRectForBounds(bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
-        var rect = super.textRectForBounds(UIEdgeInsetsInsetRect(bounds, self.padding), limitedToNumberOfLines: numberOfLines)
-        rect.origin.x += padding.left
-        rect.origin.y += padding.top
-        rect.size.width += (padding.left + padding.right)
-        rect.size.height += (padding.top + padding.bottom)
-        return rect
+        if let padding = self.padding {
+            var rect = super.textRectForBounds(UIEdgeInsetsInsetRect(bounds, padding), limitedToNumberOfLines: numberOfLines)
+            rect.origin.x += padding.left
+            rect.origin.y += padding.top
+            rect.size.width += (padding.left + padding.right)
+            rect.size.height += (padding.top + padding.bottom)
+            return rect
+        }
+        return super.textRectForBounds(bounds, limitedToNumberOfLines: numberOfLines)
     }
-
+    
 }

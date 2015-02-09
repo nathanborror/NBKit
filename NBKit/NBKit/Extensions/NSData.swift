fb05fa14d3fe7c5a85447631a80b51993e832f5a
Nathan Borror <nathan@nathanborror.com>
2015-02-09 14:26:32 -0800

    [Swift 1.2] Improved loadAsync

--- NBKit/NBKit/Extensions/NSData.swift
+++ NBKit/NBKit/Extensions/NSData.swift
@@ -10,33 +10,27 @@ import Foundation
 
 extension NSData {
 
-    class func loadAsync(url:NSURL, completion:(NSData?, NSError?) -> Void) {
-        // Check the cache to see if the item already exists
-        if let data = Cache.shared.get(url.absoluteString!) as? NSData {
-            completion(data, nil)
-            return
-        }
-
-        // TODO: Guard against multiple requests for the same url
-
-        let queue = dispatch_queue_create("NSData.loadAsyncQueue", nil)
-        dispatch_async(queue, { () -> Void in
-            var error:NSError?
-            let data = NSData(contentsOfURL: url, options: NSDataReadingOptions(0), error:&error)
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                if data != nil {
-                    Cache.shared.set(url.absoluteString!, obj: data!)
-                }
-                completion(data, error)
-            })
-        })
-    }
-
     class func loadAsync(url:String, completion:(NSData?, NSError?) -> Void) {
         if let url = NSURL(string: url) {
-            NSData.loadAsync(url, completion: completion)
-        } else {
-            Log.warn("Unable to convert: \(url) to NSURL.")
+            // Check the cache to see if the item already exists
+            if let data = Cache.shared.get(url.absoluteString!) as? NSData {
+                completion(data, nil)
+                return
+            }
+
+            // TODO: Guard against multiple requests for the same url
+
+            let queue = dispatch_queue_create("NSData.loadAsyncQueue", nil)
+            dispatch_async(queue, { () -> Void in
+                var error:NSError?
+                let data = NSData(contentsOfURL: url, options: NSDataReadingOptions(0), error:&error)
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    if data != nil {
+                        Cache.shared.set(url.absoluteString!, obj: data!)
+                    }
+                    completion(data, error)
+                })
+            })
         }
     }
 
